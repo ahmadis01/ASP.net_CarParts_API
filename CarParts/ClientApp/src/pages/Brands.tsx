bf3d56d011ae4783a7cb87047c7594abb42d3d18
Mohammad Khayata <Mohammad_Khayata@outlook.com>
2022-12-25 21:07:52 +0300

    fix typescript issues

--- CarParts/ClientApp/src/pages/Brands.tsx
+++ CarParts/ClientApp/src/pages/Brands.tsx
@@ -4,11 +4,12 @@ import { fetchBrands } from '../store/brands'
 import Grid from '@mui/material/Grid'
 import { Box, Button, Card } from '@mui/material'
 import { borderRadius } from '@mui/system'
+import { AppDispatch, RootState } from '../store'
 
 
 export default function Brands() {
-  const brands = useSelector<any>((state) => state.brand.brands) as []
-  const dispatch = useDispatch<any>()
+  const brands = useSelector<RootState>((state) => state.brand.brands) as []
+  const dispatch = useDispatch<AppDispatch>();
   useEffect(() => {
     dispatch(fetchBrands());
   }, [])
@@ -22,13 +23,13 @@ export default function Brands() {
             return (
               <div>
 
-                <Card className='p-4 flex items-center justify-center relative' sx={{borderRadius:'15px'}}  elevation={5}>
-                  <img width={150}  src={`/public/brands/${b.name}.png`}   alt="" />
+                <Card className='p-4 flex items-center justify-center relative' sx={{ borderRadius: '15px' }} elevation={5}>
+                  <img width={150} src={`/public/brands/${b.name}.png`} alt="" />
 
                   <Box sx={{ transition: '0.5s' }} className="absolute h-full w-full brand-info bg-white flex justify-center items-center text-2xl bg-opacity-70 opacity-0 hover:opacity-100 backdrop-blur-sm ">
                     <Button size='large' color='primary' sx={{ borderRadius: '15px', fontSize: '20px', fontWeight: "bold" }}>
 
-                      Show Parts
+                      عرض القطع
                     </Button>
                   </Box>
                 </Card>
