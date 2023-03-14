3d907f82da9fecfbc48fc53b10b858100196febd
AhmadIsmail <ahmad99net4@gmail.com>
2023-03-14 18:02:39 +0300

    fixes

--- CarParts/Dto/BrandDto/GetBrandDto.cs
+++ CarParts/Dto/BrandDto/GetBrandDto.cs
@@ -7,8 +7,8 @@ namespace CarParts.Dto.BrandDto
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
-        public string Image { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
         public int CountryId { get; set; }
     }
 }
--- CarParts/Models/Base/BaseProp.cs
+++ CarParts/Models/Base/BaseProp.cs
@@ -3,7 +3,7 @@
     public class BaseProp
     {
         public int Id { get; set; }
-        public DateTime CreatedAt { get; set; }
+        public DateTime? CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
         public DateTime? DeletedAt { get; set; }
     }
